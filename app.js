d4868167b09321ecc738882bb083bc7c803b4349
Mert  Kaim <kaim.mrt@gmail.com>
2021-12-25 12:47:07 +0300

    first commit
    first commit

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
 
 const app = express()
 
+//Template Engine
+app.set("view engine", "ejs")
+
+//Middlewares
+app.use(express.static('public'))
+
+//Routes 
 app.get('/', (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog)
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post')
 })
 
 const port = 3000;
